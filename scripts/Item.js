958dff4e52ff7b76dd01ac4eb2cf51826d5ef750
Martha Plantz <maplantz89@gmail.com>
2019-06-10 08:35:25 -0700

    finished #1 in namespacing

--- scripts/Item.js
+++ scripts/Item.js
@@ -4,15 +4,23 @@
 // eslint-disable-next-line no-unused-vars
 
 const Item = (function (){
-  return {};
+  const validateName =function(name){
+    if (name.length === 0 ){
+      throw TypeError('Name does not exist');
+    }
+  }
+  const create = function(name){
+    return {
+      id: cuid(),
+      name: name,
+      checked: false,
+    }
+  }
+  
+  
+  return {
+    validateName,
+    create,
+  };
 }());
 
-function validateName(name){
-  if (name.length === 0 ){
-    throw TypeError('Name does not exist');
-  }
-}
-
-function create (name){
-
-}
--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,16 @@
 
 function main() {
   shoppingList.bindEventListeners();
+
+  const itemNames = [ '', 'apples', 'pears' ];
+  itemNames.forEach(name => {
+    try {
+      Item.validateName(name);
+      store.items.push(Item.create(name));
+    } catch(error) {
+      console.log('Cannot add item: ' + error.message);
+    }
+  });
   shoppingList.render();
 }
 
